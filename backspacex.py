65e0b7ef5187f7eef9d8ff52510f0e275d3af40d
mcunbagy <99417943+mcunbagy@users.noreply.github.com>
2022-02-10 19:22:28 +0300

    Update backspacex.py

--- backspacex.py
+++ backspacex.py
@@ -12,40 +12,40 @@ class backspacex:
         #komsu renklerin puanlarini bul
         for tup in self.colors.values(): #.values()
           try:
-            if tuple([img[myinfo[0][0]+50,myinfo[0][1]]]) in tup : global puan1 = tup[1]
+            if tuple([img[myinfo[0][0]+50,myinfo[0][1]]]) in tup : puan1 = tup[1]
           except : pass
           try:
-            if tuple([img[[myinfo[0][0], myinfo[0][1]+50]]]) in tup : global puan2 = tup[1]
+            if tuple([img[[myinfo[0][0], myinfo[0][1]+50]]]) in tup : puan2 = tup[1]
           except : pass
           try:
-            if tuple([img[myinfo[0][0]-50, myinfo[0][1]]]) in tup : global puan3 = tup[1]
+            if tuple([img[myinfo[0][0]-50, myinfo[0][1]]]) in tup : puan3 = tup[1]
           except : pass
           try:
-            if tuple([img[myinfo[0][0], myinfo[0][1]-50]]) in tup : global puan4 = tup[1]
+            if tuple([img[myinfo[0][0], myinfo[0][1]-50]]) in tup :  puan4 = tup[1]
           except : pass
           try:
-            if tuple([img[myinfo[0][0]+50,myinfo[0][1]+50]]) in tup : global puan5 = tup[1]
+            if tuple([img[myinfo[0][0]+50,myinfo[0][1]+50]]) in tup :  puan5 = tup[1]
           except : pass
           try:
-            if tuple([img[myinfo[0][0]-50,myinfo[0][1]-50]]) in tup :  global puan6 = tup[1]
+            if tuple([img[myinfo[0][0]-50,myinfo[0][1]-50]]) in tup :   puan6 = tup[1]
           except : pass
           try:
-            if tuple([img[myinfo[0][0]+50,myinfo[0][1]-50]]) in tup : global puan7 = tup[1]
+            if tuple([img[myinfo[0][0]+50,myinfo[0][1]-50]]) in tup :  puan7 = tup[1]
           except : pass
           try:
-            if tuple([img[myinfo[0][0]-50,myinfo[0][1]+50]]) in tup : global puan8 = tup[1]
+            if tuple([img[myinfo[0][0]-50,myinfo[0][1]+50]]) in tup :  puan8 = tup[1]
           except : pass
           try:
-            if tuple([img[myinfo[0][0]+100,myinfo[0][1]]]) in tup : global puan9 = tup[1]
+            if tuple([img[myinfo[0][0]+100,myinfo[0][1]]]) in tup :  puan9 = tup[1]
           except : pass
           try:
-            if tuple([img[myinfo[0][0],myinfo[0][1]+100]]) in tup : global puan10 = tup[1]
+            if tuple([img[myinfo[0][0],myinfo[0][1]+100]]) in tup :  puan10 = tup[1]
           except : pass
           try:
-            if tuple([img[myinfo[0][0]-100,myinfo[0][1]]]) in tup : global puan11 = tup[1]
+            if tuple([img[myinfo[0][0]-100,myinfo[0][1]]]) in tup :  puan11 = tup[1]
           except : pass
           try:
-            if tuple([img[myinfo[0][0],myinfo[0][1]-100]]) in tup : global puan12 = tup[1]
+            if tuple([img[myinfo[0][0],myinfo[0][1]-100]]) in tup :  puan12 = tup[1]
           except : pass
 
         #hedef noktasini bul ve oraya git  
