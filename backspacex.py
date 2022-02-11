feeccf9ae971dacf34e10628eb9ab3e0df50c27f
mcunbagy <99417943+mcunbagy@users.noreply.github.com>
2022-02-11 12:34:34 +0300

    Update backspacex.py

--- backspacex.py
+++ backspacex.py
@@ -23,30 +23,52 @@ class oyuncuclass:
         for tup in renkler:
           if (y+50 < 750):
             if tuple(img[y+50,x]) in tup: puan1 = tup[1]
-          else:
-            puan1=0
+            else:puan1=0
+          else:puan1=0
           if (x+50 < 750):
             if tuple(img[y, x+50]) in tup: puan2 = tup[1]
+            else:puan2=0
+          else:puan2=0
           if (y - 50 > 0 ):          
             if tuple(img[y-50, x]) in tup: puan3 = tup[1]
+            else:puan3=0
+          else:puan3=0
           if (x-50 >0 ):
             if tuple(img[y, x-50]) in tup: puan4 = tup[1]
+            else:puan4=0
+          else:puan4=0
           if (y+50 < 750) and (x+50 < 750):
             if tuple(img[y+50, x+50]) in tup: puan5 = tup[1]
+            else:puan5=0
+          else:puan5=0
           if (y-50 >0) and (x-50 >0):
             if tuple(img[y-50, x-50]) in tup: puan6 = tup[1]
+            else:puan6=0
+          else:puan6=0
           if (y+50 < 750) and (x-50 >0):
             if tuple(img[y+50, x-50]) in tup: puan7 = tup[1]
+            else:puan7=0
+          else:puan7=0
           if (y-50 >0) and (x+50 < 750):
             if tuple(img[y-50, x+50]) in tup: puan8 = tup[1]
+            else:puan8=0
+          else:puan8=0
           if (y+100<750):
             if tuple(img[y+100,x]) in tup: puan9 = tup[1]
+            else:puan9=0
+          else:puan9=0
           if (x+100 < 750):
             if tuple(img[y,x+100]) in tup: puan10 = tup[1]
+            else:puan10=0
+          else:puan10=0
           if (y-100 > 0):
             if tuple(img[y-100,x]) in tup: puan11 = tup[1]
+            else:puan11=0
+          else:puan11=0
           if (x-100 > 0):
             if tuple(img[y, x-100]) in tup: puan12 = tup[1]
+            else:puan12=0
+          else:puan12=0
         
         #hedef noktasini bul ve oraya git  
         hedef = max(komsupuanlar)
